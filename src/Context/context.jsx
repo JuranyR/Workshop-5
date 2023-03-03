620e84dc18e2893a885edbb75746609d4761aa50
JuranyR <adelerapi95@gmail.com>
2023-03-03 10:47:48 -0500

    creation branch

--- src/Context/context.jsx
+++ src/Context/context.jsx
@@ -5,7 +5,8 @@ export const Context = createContext({})
 const ContextProvider = (props) => { 
     const pizzas =[
         {id:1, name:'peperomi', price: 3200,},
-        {id:2, name: 'hawaiana', price: 6200}
+        {id:2, name: 'hawaiana', price: 6200},
+        {id:3, name: 'carnes', price: 4500},
     ]
 
   return (
