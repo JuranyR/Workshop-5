dc24838790db51b1afd88c3d1eacd4eeace17367
dpaeza <danipaez1098@gmail.com>
2023-03-10 23:36:58 -0500

    add payment confirmation page: layout and functionalities

--- src/components/PaymentConfirmation/PaymentConfirmation.jsx
+++ src/components/PaymentConfirmation/PaymentConfirmation.jsx
@@ -1,8 +1,19 @@
+import LottieAnimation from '../lottieAnimation/LottieAnimation';
+import { Link } from 'react-router-dom';
 
 const PaymentConfirmation = () => {
 
     return (
-        <></>
+        <section className="paymentConfirmation">
+            <div className="paymentConfirmation_div1">
+                <LottieAnimation play={true} />
+                <h1>Tu pedido está en proceso</h1>
+                <p>Serás notificado cuando llegue el repartidor.</p>                
+            </div>
+            <div className='button_container'>
+                <Link to="/"><button>Aceptar</button></Link>
+                </div>
+        </section>
     )
 }
 
